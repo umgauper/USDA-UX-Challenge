9c6c27244614033e0ef1f0b400910fedd67b2604
Una Gauper <umgauper@gmail.com>
2016-02-24 11:35:26 -0800

    added UI interactivity to childIncome section

--- src/components/ChildIncomeQuestions/ChildIncomeQuestion.js
+++ src/components/ChildIncomeQuestions/ChildIncomeQuestion.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+	import React, { Component } from 'react'
 import IncomeInput from './IncomeInput'
 
 export default class ChildIncomeQuestion extends Component {
@@ -6,7 +6,8 @@ export default class ChildIncomeQuestion extends Component {
 		super(props)
 		this.state = {
 			amt: null,
-			frequency: null
+			frequency: null,
+			activeIndex: 0
 		}
 	}
 
@@ -14,18 +15,23 @@ export default class ChildIncomeQuestion extends Component {
 		let question = this.props.question
 		let names = this.props.names
 
-	
+		let increaseActiveIndex = () => {
+			this.setState({activeIndex: this.state.activeIndex + 1})
+		}
 		
 		return (
 			<div>
 				
 				{names.map( (name, i) => {
-					return (<div> 
+					return (<div style={ this.state.activeIndex === i ? {display: 'block'} : {display: 'none'} }> 
 								<p>{question(name)}</p>
 								<IncomeInput 
 									i={i}
 									incomeCategory={this.props.incomeCategory}
 									onNextClick={this.props.onNextClick}
+									increaseActiveIndex={increaseActiveIndex}
+									numberOfNames={names.length}
+									activeIndex={this.state.activeIndex}
 									/>
 							</div>
 							)
--- src/components/ChildIncomeQuestions/IncomeInput.js
+++ src/components/ChildIncomeQuestions/IncomeInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 
 class IncomeInput extends Component {
@@ -30,7 +31,7 @@ class IncomeInput extends Component {
 			this.setState({showInput: true})
 		}
 
-			let radioClickHandler = (e) => {
+		let radioClickHandler = (e) => {
 			this.setState({frequency: e.target.value})
 		}
 
@@ -48,26 +49,48 @@ class IncomeInput extends Component {
 
 		let i = this.props.i
 
-		let clickHandler = (i) => {
+		let nextClickHandler = (e, i) => {
+			if (this.props.activeIndex !== this.props.numberOfNames -1 ) {
+				e.preventDefault() // don't go to a new path unless we're on the last name in the list of child names
+			}
 			console.log(makeChildIncomeObject())
 			this.props.onNextClick(makeChildIncomeObject(), i)
+
+			this.props.increaseActiveIndex()
 		}
 
+		let noClickHandler = (e) => {
+			if (this.props.activeIndex !== this.props.numberOfNames - 1 ) {
+				e.preventDefault() // don't go to a new path unless we're on the last name in the list of child names
+			}
+
+			this.props.increaseActiveIndex()
+		}
+
+		let nextPath = Number.parseInt(window.location.hash.match(/childIncome\/?([0-9]+)/)[1]) + 1
 
 		return (
 			<div>
 				<button onClick={yesClickHandler}>Yes</button>
-				<button>No</button>
+				<Link to={nextPath === 12 ? "adultIncome/12" : "childIncome/" + nextPath}
+							onClick={ (e) => {noClickHandler(e)} }>
+					<button>No</button>
+				</Link>
 				<div style={ this.state.showInput ? {display: 'block'} : {display: 'none'}}>
 					<p>Please enter the dollar amount of the money received and select the frequency of the amount</p>
 						<input type="radio" value="weekly" name={i} onClick={(e) => {radioClickHandler(e)}}/>
 						<input type="radio" value="twiceweekly" name={i} onClick={(e) => {radioClickHandler(e)}}/>
 						<input type="radio" value="monthly" name={i} onClick={(e) => {radioClickHandler(e)}}/>
 						<input type="radio" value="twicemonthly" name={i} onClick={(e) => {radioClickHandler(e)}}/>
+						<Link 
+							to={nextPath === 12 ? "adultIncome/12" : "childIncome/" + nextPath}
+							onClick={ (e) => {nextClickHandler(e, i)} }
+							>
 						<button 
 							disabled={!this.state.frequency || !this.state.amt || this.state.showErrorMessage} 
-							onClick={ () => {clickHandler(i)} }>NEXT
+							>NEXT
 						</button>
+						</Link>
 					<input type="text" name="amt" onChange={(e) => inputChangeHandler(e)}/>
 					<span style={this.state.showErrorMessage ? {display: 'block'} : {display: 'none'}}>{errorMessage}</span>
 				</div>		
